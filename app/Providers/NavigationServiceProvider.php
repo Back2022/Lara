a10548da23b3170af0f49d640dc40545d58015d1
mrvic <mrvic.predrag@gmail.com>
2022-02-14 19:58:29 +0100

    NAd svim viewovima service provider Navigation generira linkove

--- app/Providers/NavigationServiceProvider.php
+++ app/Providers/NavigationServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+
+// $ php artisan make:provider NavigationServiceProvider
+
 class NavigationServiceProvider extends ServiceProvider
 {
     /**
@@ -18,11 +21,24 @@ class NavigationServiceProvider extends ServiceProvider
 
     /**
      * Bootstrap services.
+     * Prilikom prikaza bilo kojeg viewa, 
+     * mapiraj varijablu polje linkova
      *
      * @return void
      */
     public function boot()
     {
-        //
+     
+       // preko fascade
+      //  \Illuminate\Support\Facades\View::class
+       // view()->composer('ispod.*', function (\Illuminate\View\View $view){
+        view()->composer('*', function (\Illuminate\View\View $view){
+        $link1="http:\\\\www.google.com";
+        $link2="http:\\\\www.google.com";        
+        $link3="http:\\\\www.google.com";   
+            $arr=compact('link1','link2','link3');
+            return $view->with('arr',$arr);
+                   
+        });
     }
 }
