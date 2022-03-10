1e205d4d4e934629634cd46176361ceb787b48cd
mrvic <mrvic.predrag@gmail.com>
2022-03-10 19:51:53 +0100

    Rsync primjer

--- lara2022.sql
+++ lara2022.sql
@@ -155,7 +155,7 @@ CREATE TABLE `migrations` (
   `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -164,7 +164,7 @@ CREATE TABLE `migrations` (
 
 LOCK TABLES `migrations` WRITE;
 /*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
-INSERT INTO `migrations` VALUES (9,'2014_10_12_000000_create_users_table',1),(10,'2014_10_12_100000_create_password_resets_table',1),(11,'2019_08_19_000000_create_failed_jobs_table',1),(12,'2019_12_14_000001_create_personal_access_tokens_table',1),(13,'2022_02_16_184318_create_froots',1),(14,'2022_02_21_190904_create_offices_table',1),(15,'2022_02_24_190924_create_employees_table',1),(16,'2022_02_28_173934_create_customers_table',1);
+INSERT INTO `migrations` VALUES (17,'2014_10_12_000000_create_users_table',1),(18,'2014_10_12_100000_create_password_resets_table',1),(19,'2019_08_19_000000_create_failed_jobs_table',1),(20,'2019_12_14_000001_create_personal_access_tokens_table',1),(21,'2022_02_16_184318_create_froots',1),(22,'2022_02_21_190904_create_offices_table',1),(23,'2022_02_24_190924_create_employees_table',1),(24,'2022_02_28_173934_create_customers_table',1);
 /*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -197,7 +197,7 @@ CREATE TABLE `offices` (
 
 LOCK TABLES `offices` WRITE;
 /*!40000 ALTER TABLE `offices` DISABLE KEYS */;
-INSERT INTO `offices` VALUES ('1','San Francisco','+1 650 219 4782','100 Market Street','Suite 300','CA','USA','94080','NA','2022-03-08 17:42:15','2022-03-08 17:42:15'),('2','Boston','+1 215 837 0825','1550 Court Place','Suite 102','MA','USA','02107','NA','2022-03-08 17:42:15','2022-03-08 17:42:15'),('3','NYC','+1 212 555 3000','523 East 53rd Street','apt. 5A','NY','USA','10022','NA','2022-03-08 17:42:15','2022-03-08 17:42:15'),('4','Paris','+33 14 723 4404','43 Rue Jouffroy D\'abbans',NULL,NULL,'France','75017','EMEA','2022-03-08 17:42:15','2022-03-08 17:42:15'),('5','Tokyo','+81 33 224 5000','4-1 Kioicho',NULL,'Chiyoda-Ku','Japan','102-8578','Japan','2022-03-08 17:42:15','2022-03-08 17:42:15'),('6','Sydney','+61 2 9264 2451','5-11 Wentworth Avenue','Floor #2',NULL,'Australia','NSW 2010','APAC','2022-03-08 17:42:15','2022-03-08 17:42:15'),('7','London','+44 20 7877 2041','25 Old Broad Street','Level 7',NULL,'UK','EC2N 1HN','EMEA','2022-03-08 17:42:15','2022-03-08 17:42:15');
+INSERT INTO `offices` VALUES ('1','San Francisco','+1 650 219 4782','100 Market Street','Suite 300','CA','USA','94080','NA','2022-03-09 17:01:47','2022-03-09 17:01:47'),('2','Boston','+1 215 837 0825','1550 Court Place','Suite 102','MA','USA','02107','NA','2022-03-09 17:01:47','2022-03-09 17:01:47'),('3','NYC','+1 212 555 3000','523 East 53rd Street','apt. 5A','NY','USA','10022','NA','2022-03-09 17:01:47','2022-03-09 17:01:47'),('4','Paris','+33 14 723 4404','43 Rue Jouffroy D\'abbans',NULL,NULL,'France','75017','EMEA','2022-03-09 17:01:47','2022-03-09 17:01:47'),('5','Tokyo','+81 33 224 5000','4-1 Kioicho',NULL,'Chiyoda-Ku','Japan','102-8578','Japan','2022-03-09 17:01:47','2022-03-09 17:01:47'),('6','Sydney','+61 2 9264 2451','5-11 Wentworth Avenue','Floor #2',NULL,'Australia','NSW 2010','APAC','2022-03-09 17:01:47','2022-03-09 17:01:47'),('7','London','+44 20 7877 2041','25 Old Broad Street','Level 7',NULL,'UK','EC2N 1HN','EMEA','2022-03-09 17:01:47','2022-03-09 17:01:47');
 /*!40000 ALTER TABLE `offices` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -275,7 +275,7 @@ CREATE TABLE `users` (
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_email_unique` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -284,7 +284,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'pmrvic@789.com','pmrvic@789.com',NULL,'$2y$10$c6hnX62VscENYOLoV45DLuIhX/q4lvxwyLnGap9eE2KdJOdlaM8ay','2022-03-02 17:55:43','2022-03-02 15:55:17','2022-03-02 15:55:17'),(2,'test','asdsafr','2022-03-08 18:44:45','dfds','2022-03-08 19:45:01',NULL,NULL);
+INSERT INTO `users` VALUES (1,'pmrvic@789.com','pmrvic@789.com',NULL,'$2y$10$c6hnX62VscENYOLoV45DLuIhX/q4lvxwyLnGap9eE2KdJOdlaM8ay','2022-03-02 17:55:43','2022-03-02 15:55:17','2022-03-02 15:55:17');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -297,4 +297,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-03-08 21:11:05
+-- Dump completed on 2022-03-09 20:29:04
---
+++ rsync.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+        <div>Ovo otpakiraj u C:\Program Files\Git\usr\bin i restartiraj git bash</div>
+        <a href="rsync_for_git_bash.zip">DOwnload RSYNC for GIT Bash</a>
+        <p>
+            rsync -avz -e "ssh -i /c/Users/Korisnik/Downloads/amazonlara2022.pem" --progress ./dir1 ubuntu@18.212.32.81:/var/www/html
+        </p>
+        
+    </body>
+</html>
