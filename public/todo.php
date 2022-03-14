7dc0f1136de0976f4a6c39a7afb292f10484b7a5
mrvic <mrvic.predrag@gmail.com>
2022-03-14 18:17:27 +0100

    dodan Procfile za Heroku

---
+++ public/todo.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>    UPDAJTAJ NODE SA ubuntu@ip-172-31-85-176:/var/www/lara$ node -v
+v10.19.0
+
+Na min V.12</h1><h2>
+            zavrsi instalaciju breeze
+    https://laravel.com/docs/9.x/starter-kits#laravel-breeze
+        </h2>
+        
+        <?php
+        //todo update node sa 10 na min 12.5
+        /*
+Error: You are using an unsupported version of Node. Please update to at least Node v12.14
+
+         * ubuntu@ip-172-31-85-176:/var/www/lara$ node -v
+v10.19.0
+
+         * 
+         *          */
+        ?>
+    </body>
+</html>
